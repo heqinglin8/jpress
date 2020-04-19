b5d70aa41faa8ff5eecf46eb997f7ec6c834da81
heqinglin <heqinglin@autohome.com.cn>
2020-04-19 23:43:57 +0800

    完成表单相关后台开发

---
+++ module-form/module-form-web/src/main/java/io/jpress/module/form/FormModuleInitializer.java
@@ -0,0 +1,72 @@
+/**
+ * Copyright (c) 2016-2020, Michael Yang 杨福海 (fuhai999@gmail.com).
+ * <p>
+ * Licensed under the GNU Lesser General Public License (LGPL) ,Version 3.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.gnu.org/licenses/lgpl-3.0.txt
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.jpress.module.form;
+
+import com.jfinal.core.Controller;
+import com.jfinal.template.Engine;
+import io.jboot.core.listener.JbootAppListenerBase;
+import io.jpress.core.menu.MenuGroup;
+import io.jpress.core.module.ModuleListener;
+
+import java.util.List;
+
+/**
+ * @author Michael Yang 杨福海 （fuhai999@gmail.com）
+ * @version V1.0
+ * @Title: 应用启动监听器
+ * @Package io.jpress.module.page
+ */
+public class FormModuleInitializer extends JbootAppListenerBase implements ModuleListener {
+
+
+    @Override
+    public String onRenderDashboardBox(Controller controller) {
+        return null;
+    }
+
+    @Override
+    public String onRenderToolsBox(Controller controller) {
+        return null;
+    }
+
+    @Override
+    public void onConfigUcenterMenu(List<MenuGroup> ucenterMenus) {
+
+    }
+
+
+    @Override
+    public void onConfigAdminMenu(List<MenuGroup> adminMenus) {
+
+        MenuGroup menuGroup = new MenuGroup();
+        menuGroup.setId("form");
+        menuGroup.setText("表单");
+        menuGroup.setIcon("<i class=\"fa fa-fw fa-file\"></i>");
+        menuGroup.setOrder(3);
+
+        adminMenus.add(menuGroup);
+
+    }
+
+
+
+    @Override
+    public void onEngineConfig(Engine engine) {
+        engine.addSharedFunction("/WEB-INF/views/commons/page/defaultPageCommentPage.html");
+    }
+
+
+}
---
+++ module-form/module-form-web/src/main/java/io/jpress/module/form/controller/_FormController.java
@@ -0,0 +1,69 @@
+/**
+ * Copyright (c) 2016-2020, Michael Yang 杨福海 (fuhai999@gmail.com).
+ * <p>
+ * Licensed under the GNU Lesser General Public License (LGPL) ,Version 3.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.gnu.org/licenses/lgpl-3.0.txt
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.jpress.module.form.controller;
+import com.jfinal.aop.Inject;
+import com.jfinal.plugin.activerecord.Page;
+import io.jboot.utils.StrUtil;
+import io.jboot.web.controller.annotation.RequestMapping;
+import io.jpress.JPressConsts;
+import io.jpress.core.menu.annotation.AdminMenu;
+import io.jpress.module.form.model.FormInfo;
+import io.jpress.module.form.model.FormInfoExt;
+import io.jpress.module.form.service.FormInfoService;
+import io.jpress.web.base.AdminControllerBase;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Michael Yang 杨福海 （fuhai999@gmail.com）
+ * @version V1.0
+ * @Package io.jpress.module.page.controller.admin
+ */
+@RequestMapping(value = "/admin/form", viewPath = JPressConsts.DEFAULT_ADMIN_VIEW)
+public class _FormController extends AdminControllerBase {
+
+    @Inject
+    private FormInfoService fis;
+
+    @AdminMenu(text = "表单管理", groupId = "form", order = 1)
+    public void index() {
+
+
+        List<FormInfo> pages = fis.findAll();
+        List<FormInfoExt> forms = new ArrayList<>();
+        if(pages!=null && !pages.isEmpty()){
+            for(int i=0;i<pages.size();i++){
+                FormInfo formInfo = pages.get(i);
+                forms.add(new FormInfoExt(formInfo));
+            }
+        }
+        setAttr("forms", forms);
+
+//        int draftCount = sps.findCountByStatus(SinglePage.STATUS_DRAFT);
+//        int trashCount = sps.findCountByStatus(SinglePage.STATUS_TRASH);
+//        int normalCount = sps.findCountByStatus(SinglePage.STATUS_NORMAL);
+//
+//        setAttr("draftCount", draftCount);
+//        setAttr("trashCount", trashCount);
+//        setAttr("normalCount", normalCount);
+//        setAttr("totalCount", draftCount + trashCount + normalCount);
+
+        render("form/form_list.html");
+    }
+
+
+}
